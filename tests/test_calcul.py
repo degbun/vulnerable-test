17e5f068746c80cc9a2dbb0b17671d595df29548
Degbun Josue <78054512+degbun@users.noreply.github.com>
2025-01-29 15:04:24 +0000

    Update test_calcul.py

--- tests/test_calcul.py
+++ tests/test_calcul.py
@@ -8,3 +8,9 @@ def test_addition():
 def test_soustraction():
     assert soustraction(5, 3) == 2
     assert soustraction(0, 0) == 0
+
+
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+    assert multiplication(-1, 1) == -1
+    assert multiplication(0, 5) == 0
