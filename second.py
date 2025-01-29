2c1eeabb3aea4f07cdf48400fd9dd39ea0503198
Degbun Josue <78054512+degbun@users.noreply.github.com>
2025-01-29 11:24:27 +0000

    Create second.py

---
+++ second.py
@@ -0,0 +1,12 @@
+def addition(a, b):
+    """
+    Cette fonction prend deux nombres en entrée et retourne leur somme.
+    """
+    return a + b
+
+
+def multiplication(a, b):
+    """
+    Cette fonction prend deux nombres en entrée et retourne leur produit.
+    """
+    return a * b
